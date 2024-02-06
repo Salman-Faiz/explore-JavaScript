9accac970dadf3e1986d660ba1f9af50c8d02d8e
Salman-Faiz <salmanfaiz.cse@gmail.com>
2024-02-06 22:35:40 +0600

    largest Number

---
+++ largestNumber.js
@@ -0,0 +1,14 @@
+var a=13;
+var b=45;
+var c=79;
+var largest =79;
+
+if(a == largest){
+    console.log('13 is largest');
+}
+else if( b==largest){
+    console.log('45 is largest'); 
+}
+else if( c==largest){
+    console.log('79 is largest'); 
+}
