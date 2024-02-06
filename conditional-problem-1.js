f6f2c6597f45e709abc5de44f78769dc024c65ed
Salman-Faiz <salmanfaiz.cse@gmail.com>
2024-02-06 22:08:27 +0600

    conditional problem1 solved

---
+++ conditional-problem-1.js
@@ -0,0 +1,92 @@
+
+var salman = 85;
+var tom = 66;
+var jane = 95;
+var peter = 56;
+var john = 40;
+
+
+
+if(salman>=90 && salman<=100 ) {
+    console.log('salman got A+');
+}
+else if(tom>=90 && tom<=100 ){
+    console.log('tom got A+');
+}
+else if(jane>=90 && jane<=100 ){
+    console.log('jane got A+');
+}
+else if(peter>=90 && peter<=100 ){
+    console.log('peter got A+');
+}
+else if(john>=90 && john<=100 ){
+    console.log('john got A+');
+}
+
+
+if(salman>=80 && salman<=89 ) {
+    console.log('salman got A');
+}
+else if(tom>=80 && tom<=89 ){
+    console.log('tom got A');
+}
+else if(jane>=80 && jane<=89 ){
+    console.log('jane got A');
+}
+else if(peter>=80 && peter<=89 ){
+    console.log('peter got A');
+}
+else if(john>=80 && john<=89 ){
+    console.log('john got A');
+}
+
+if(salman>=60 && salman<=79){
+    console.log('salman got B');
+}
+else if(tom>=60 && tom<=79){
+    console.log('tom got B');
+}
+else if(jane>=60 && jane<=79){
+    console.log('jane got B');
+}
+else if(peter>=60 && peter<=79){
+    console.log('peter got B');
+}
+else if(john>=60 && john<=79){
+    console.log('tom got B');
+}
+
+
+
+if(salman>=50 && salman<=59){
+    console.log('salman got C');
+}
+else if(tom>=50 && tom<=59){
+    console.log('tom got C');
+}
+else if(jane>=50 && jane<=59){
+    console.log('jane got C');
+}
+else if(peter>=50 && peter<=59){
+    console.log('peter got C');
+}
+else if(john>=50 && john<=59){
+    console.log('tom got C');
+}
+
+
+if(salman>=40 && salman<=49){
+    console.log('salman got D');
+}
+else if(tom>=40 && tom<=49){
+    console.log('tom got D');
+}
+else if(jane>=40 && jane<=49){
+    console.log('jane got D');
+}
+else if(peter>=40 && peter<=49){
+    console.log('peter got D');
+}
+else if(john>=40 && john<=49){
+    console.log('john got D');
+}
