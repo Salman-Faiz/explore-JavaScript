f188dfb6f3854898abf2102ce5e87d68f98b5ce6
Salman-Faiz <salmanfaiz.cse@gmail.com>
2024-02-06 22:43:15 +0600

    isosceles triangle

---
+++ triangle.js
@@ -0,0 +1,13 @@
+var a=8;
+var b=8;
+var c=9;
+
+if(a===b && b!==c){
+    console.log('this is a isosceles triangle');
+}
+else if(a===c && b!==a){
+    console.log('this is a isosceles triangle');
+}
+else{
+    console.log('this is not a isosceles triangle');
+}
