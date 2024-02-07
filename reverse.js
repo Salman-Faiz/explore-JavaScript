c4c6ed7edb3e0d04c1c67dc8a5050c08007162fb
Salman-Faiz <salmanfaiz.cse@gmail.com>
2024-02-07 16:06:41 +0600

    reverse while loop

---
+++ reverse.js
@@ -0,0 +1,12 @@
+var num =10;
+while(num >=1 ){
+    console.log(num)
+    num--;
+}
+
+var names=['sayed','salman','faiz'];
+
+for(var i=names.length; i>=0; i--){
+    var myName = names[i]
+    console.log(myName);
+}
