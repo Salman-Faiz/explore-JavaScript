b67097d5addeec29fb426e4927b75a7d2015475e
Salman-Faiz <salmanfaiz.cse@gmail.com>
2024-02-08 12:50:49 +0600

    return added

--- return.js
+++ return.js
@@ -5,3 +5,13 @@ function add(num1,num2,num3){
 }
 var total = add(20,20,20);
 console.log(total);
+
+
+function singaraNiyeAso(money){
+    var potiSingaraPrice =10;
+    var singaraPabo= money/potiSingaraPrice;
+    return singaraPabo;
+}
+var amarTaka =300;
+ var sorboMotSingara = singaraNiyeAso(amarTaka); 
+ console.log('sorbo Mot Singara',sorboMotSingara);
