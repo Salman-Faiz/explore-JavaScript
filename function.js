0e0c4211ec418e7c08a2cde94147b5d1bd47b9ac
Salman-Faiz <salmanfaiz.cse@gmail.com>
2024-02-08 12:36:29 +0600

    fuctions parameters return added

---
+++ function.js
@@ -0,0 +1,9 @@
+function add(a,b,c,d,e,f){
+    console.log(a,b,c,d,e,f);
+    var sum = a+b+c+d+e+f;
+    console.log(sum);
+
+}
+var g=70;
+var h=80;
+add(10,20,30,40,g,h);
---
+++ return.js
@@ -0,0 +1,7 @@
+function add(num1,num2,num3){
+    console.log(num1, num2, num3);
+    var sum= num1+ num2+num3;
+    return sum;
+}
+var total = add(20,20,20);
+console.log(total);
